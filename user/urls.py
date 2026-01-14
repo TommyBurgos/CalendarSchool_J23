d834e21ecf97b9b2b34030e87134b0be3427b4f7
tommyBurgos <trburgos@espol.edu.ec>
2026-01-13 20:04:43 -0500

    Probando el olvidé la contraseña

--- user/urls.py
+++ user/urls.py
@@ -11,3 +11,37 @@ urlpatterns = [
     path("cambiar-password/forzado/", views.cambiar_password_forzado, name="cambiar_password_forzado"),
 
 ]
+
+from django.contrib.auth import views as auth_views
+
+urlpatterns += [
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="user/password_reset.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="user/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="user/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="user/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+]
+
